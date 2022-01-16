af6f4e4216887a63d543aa1959305d3c5fd17e5a
Yuka <yuka@YukanoMacBook-ea.local>
2022-01-16 11:51:01 +0900

    migrationfile

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name', 15);
             $table->string('phone',15);
-            $table->string('email',254)->unique();
+            $table->string('email',100)->unique();
         });
     }
 
